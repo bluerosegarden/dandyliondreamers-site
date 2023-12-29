96e54337a051949a2052d91c52d7c3e25e8e1a2d
wych <wychwitchcraft@gmail.com>
2023-12-28 21:59:32 -0600

    some quick changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,20 @@
-<script>
+<script lang="ts">
+    import data from "$lib/cms/pages/home.json";
     import Header from "$lib/components/Header.svelte";
     import "$lib/styles/web.css";
     import "$lib/app.css";
     import { fade } from "svelte/transition";
     import Footer from "$lib/components/Footer.svelte";
+    import { base } from "$app/paths";
 </script>
+
+<head>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://linkfork.co/" />
+    <meta property="og:title" content={data.title} />
+    <meta property="og:description" content={data.blurb} />
+    <meta property="og:image" content="{base}{data.embedImg}" />
+</head>
 
 <Header />
 <main
