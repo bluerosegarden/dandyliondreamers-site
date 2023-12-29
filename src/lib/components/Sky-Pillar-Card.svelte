c41bf83cc6505024c3106d525f7575ceb7d3b7a7
wych <wychwitchcraft@gmail.com>
2023-12-28 21:43:30 -0600

    add sky pillar card

---
+++ src/lib/components/Sky-Pillar-Card.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { base } from "$app/paths";
+    import { Lightbox } from "svelte-lightbox";
+    import SvelteMarkdown from "svelte-markdown";
+    export let set;
+</script>
+
+<div
+    class="p-2 px-4 bg-white/50 m-2 w-60 rounded flex flex-col align-center shadow-lg"
+>
+    <h2 class="w-fit text-center">{set.title}</h2>
+    <Lightbox>
+        <img class="w-60" src="{base}{set.img}" alt={set.title} />
+    </Lightbox>
+    <div class="mx-1 list-disc w-fit text-md">
+        <SvelteMarkdown source={set.desc} />
+    </div>
+</div>
