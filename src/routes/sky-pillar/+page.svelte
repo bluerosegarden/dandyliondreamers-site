58819cd3ae246f0a9860503edb25d881d225ae04
wych <wychwitchcraft@gmail.com>
2023-12-28 18:04:47 -0600

    rayqueza finished

--- src/routes/sky-pillar/+page.svelte
+++ src/routes/sky-pillar/+page.svelte
@@ -1,11 +1,14 @@
 <script>
-    import PortfolioSet from "$lib/components/Sky-Pillar-Card.svelte";
+    import RayquezaCard from "$lib/components/Sky-Pillar-Card.svelte";
 
     export let data;
     const sets = data.raySets;
 </script>
 
 <h1 class="text-6xl">{data.title}</h1>
-{#each sets as set}
-    <PortfolioSet {set} />
-{/each}
+
+<section class="flex flex-row flex-wrap">
+    {#each sets as set}
+        <RayquezaCard {set} />
+    {/each}
+</section>
