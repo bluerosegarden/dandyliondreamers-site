e4ce8470ca9c726c5a4d57a861fc1c37cf40bd5f
wych <wychwitchcraft@gmail.com>
2023-12-30 14:35:03 -0600

    page fade fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
-    import data from "$lib/cms/pages/main-settings.json";
+    import site_data from "$lib/cms/pages/main-settings.json";
     import Header from "$lib/components/Header.svelte";
     import "$lib/styles/web.css";
     import "$lib/app.css";
     import { fade } from "svelte/transition";
     import Footer from "$lib/components/Footer.svelte";
+    export let data;
 </script>
 
 <head>
     <meta property="og:type" content="website" />
-    <meta property="og:url" content={data.url} />
-    <meta property="og:title" content={data.title} />
-    <meta property="og:description" content={data.blurb} />
-    <meta property="og:image" content={data.embedImg} />
+    <meta property="og:url" content={site_data.url} />
+    <meta property="og:title" content={site_data.title} />
+    <meta property="og:description" content={site_data.blurb} />
+    <meta property="og:image" content={site_data.embedImg} />
 </head>
-
-<Header />
-<main
-    class="rounded text-lg sm:mx-10 md:mx-36 leading-normal"
-    in:fade={{ duration: 150, delay: 150 }}
-    out:fade={{ duration: 150 }}
->
-    <slot />
-</main>
+{#key data.pathname}
+    <Header />
+    <main
+        class="rounded text-lg sm:mx-10 md:mx-36 leading-normal"
+        in:fade={{ duration: 150, delay: 150 }}
+        out:fade={{ duration: 150 }}
+    >
+        <slot />
+    </main>
+{/key}
 <Footer />
